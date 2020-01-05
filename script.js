42e67db9afe7f8236d21324d42dbba61d62b2e06
Corina Pelichowski <corina@danitt.com>
2020-01-05 16:46:28 +1100

    tutorial complete

--- script.js
+++ script.js
@@ -40,6 +40,61 @@ function Ball() {
     this.x = Math.random() * (tx - this.radius * 2) + this.radius;
     this.y = Math.random() * (ty - this.radius);
     this.dy = Math.random() * 2;
-    this.dx = Math.random((Math.random))
-
+    this.dx = Math.random((Math.random() - 0.5) * 10);
+    this.vel = Math.random() / 5;
+    this.update = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        c.fillStyle = this.color;
+        c.fill();
+    };
 }
+
+var bal = [];
+for (var i = 0; i < 50; i++) {
+    bal.push(new Ball());
+}
+
+function animate() {
+    if (tx != window.innerWidth || ty != window.innerHeight) {
+        tx = window.innerWidth;
+        ty = windowinnerHeight;
+        canvas.width = tx;
+        canvas.width = ty;
+    }
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, tx, ty);
+    for (var i = 0; i < bal.length; i++) {
+        bal[i].update();
+        bal[i].y += bal[i].dy;
+        bal[i].x += bal[i].dx;
+        if (bal[i].y + bal[i].radius >= ty) {
+            bal[i].dy = -bal[i].dy * grav;
+        } else {
+            bal[i].dy += bal[i].vel;
+        }
+        if (bal[i].x + bal[i].radius > tx || bal[i].radius < 0) {
+            bal[i].dx = -bal[i].dx;
+        }
+        if (mousex > bal[i].x - 20 &&
+            mousex < bal[i].x + 20 &&
+            mousey > bal[i].y - 50 &&
+            mousey > bal[i].y + 50 &&
+            bal[i].radius < 70) {
+            bal[i].radius += 5;
+        } else {
+            if (bal[i].radius > bal[i].startradius) {
+                bal[i].radius = + -5;
+            }
+        }
+        //forloop end
+    }
+    //animation end
+}
+
+animate();
+
+setInterval(function () {
+    bal.push(new Ball());
+    bal.splice(0, 1);
+}, 400);
